d1ffe8c4a2d5cc2cbe34bc3d84321f8d30025bb6
Austin Valeske <avaleske@gmail.com>
2015-08-24 20:00:49 -0700

    adding a uiscrollview and uirefreshcontrol

--- istayon_ios/ViewController.m
+++ istayon_ios/ViewController.m
@@ -14,14 +14,30 @@
 
 @implementation ViewController
 
+@synthesize scrollView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"Pull to refresh" ];
+    [refreshControl addTarget:self action:@selector(refreshView:) forControlEvents:UIControlEventValueChanged];
+    
+    [self.scrollView insertSubview:refreshControl atIndex:0];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)refreshView: (UIRefreshControl *) refresh{
+    double delay = 2;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [refresh endRefreshing];
+    });
 }
 
 @end
